a67a3a9e6a7d6ccbdc109af80a3a39cb97a431b6
Grzanekkk <tofukles@gmail.com>
2021-11-23 23:47:42 +0100

    Init

--- Source/CoopStealthGame/CoopStealthGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CoopStealthGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoopStealthGame, "CoopStealthGame" );
- 
